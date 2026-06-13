10646a80706598f7051000a2121eab1379133f18
Shashikumar V <vshashikumar416@gmail.com>
2026-06-13 23:21:38 +0530

    removed throw section

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,8 +1,8 @@
 import jwt from "jsonwebtoken";
 
-if (!process.env.JWT_SECRET) {
-    throw new Error("JWT_SECRET is not defined");
-}
+// if (!process.env.JWT_SECRET) {
+//     throw new Error("JWT_SECRET is not defined");
+// }
 
 export async function authMiddleware(req, res, next){
     let token = null;
